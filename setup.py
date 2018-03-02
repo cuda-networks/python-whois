0ceb05efb49cbe105948fc6fc1b29624ec96b69e
Marco Schweighauser <mschweighauser@barracuda.com>
2018-03-01 17:44:01 -0800

    Re-licensed WTFPL -> MIT

--- setup.py
+++ setup.py
@@ -1,16 +1,15 @@
 from setuptools import setup
 
-setup(name='pythonwhois',
-      version='2.4.3',
+setup(name='pythonwhois-bn',
+      version='2.5.0',
       description='Module for retrieving and parsing the WHOIS data for a domain. Supports most domains. No dependencies.',
-      author='Sven Slootweg',
-      author_email='pythonwhois@cryto.net',
-      url='http://cryto.net/pythonwhois',
+      author='Barracuda Networks',
+      url='https://github.com/cuda-networks/python-whois',
       packages=['pythonwhois'],
       package_dir={"pythonwhois":"pythonwhois"},
       package_data={"pythonwhois":["*.dat"]},
       install_requires=['argparse'],
       provides=['pythonwhois'],
       scripts=["pwhois"],
-      license="WTFPL"
+      license="MIT"
      )
